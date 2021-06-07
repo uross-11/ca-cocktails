d0056a72c59ce344346e17f147e65fd4ac29e75d
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2021-06-07 23:45:55 +0200

    Add navbar, add error and about pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas mollitia enim consequatur quia, ipsam tempora labore obcaecati, est deserunt magni harum blanditiis in amet impedit ab aperiam nisi aliquid quasi doloremque similique, necessitatibus iste exercitationem? Ea odio quisquam deleniti sapiente!</p>
+    </section>
   )
 }
 
